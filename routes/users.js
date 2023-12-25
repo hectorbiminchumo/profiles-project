3f618f18a7ab5e720594d6ec5c899d4f97c6aa2c
Hector Biminchumo <hector.biminchumo@externos-cl.cencosud.com>
2023-12-25 00:50:31 -0500

    file upload with multer package

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const multer = require('multer');
+
+const db = require('../data/database')
+
+const storageConfig = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'images');
+  },
+  filename: function(req, file, cb) {
+    cb(null,Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storageConfig });
 
 const router = express.Router();
 
@@ -9,5 +23,18 @@ router.get('/', function(req, res) {
 router.get('/new-user', function(req, res) {
   res.render('new-user');
 });
+
+router.post('/profiles', upload.single('image'), async function(req, res){
+  const uploadedImageFile = req.file;
+  const userData = req.body;
+
+  await db.getDb().collection('users').insertOne({
+    name: userData.username,
+    imagePath: uploadedImageFile.path
+  })
+  res.redirect('/');
+
+}
+)
 
 module.exports = router;
